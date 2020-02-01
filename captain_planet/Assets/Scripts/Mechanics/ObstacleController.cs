9f25ee5e4eb6c13620f7e035e49805b0ad6f340f
Yusha16 <chris.yushi@gmail.com>
2020-02-01 11:45:06 -0500

    Add Bridge Obstacle
    Add a Obstacle Object (Bridge)
    Add a hider object for the obstacle

---
+++ captain_planet/Assets/Scripts/Mechanics/ObstacleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Must change the name the Projectle
+        if (collision.gameObject.tag == "Projectile")
+        {
+            Debug.Log("Hello");
+            //Remove the hider object
+            if (transform.childCount > 0) {
+                Destroy(transform.GetChild(0).gameObject);
+            }
+        }
+        
+
+    }
+}
