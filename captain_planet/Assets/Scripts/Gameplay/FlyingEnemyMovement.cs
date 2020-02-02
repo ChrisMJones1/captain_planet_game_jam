18669f8d07464ad93cfd77de67023d30eda60605
vietphuongtran <55298689+vietphuongtran@users.noreply.github.com>
2020-02-02 13:35:23 -0500

    enemy UI final commit
    adding movement of the enemy

---
+++ captain_planet/Assets/Scripts/Gameplay/FlyingEnemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public bool MoveLeft;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveLeft)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2 (0.3f, 0.3f);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2 (-0.3f, 0.3f);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.CompareTag("turn"))
+        {
+            if (MoveLeft)
+            {
+                MoveLeft = false;
+            }
+            else
+            {
+                MoveLeft = true;
+            }
+        }
+    }
+}
